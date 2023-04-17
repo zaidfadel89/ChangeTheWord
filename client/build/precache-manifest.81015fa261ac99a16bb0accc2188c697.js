2d70e6a8da175bcde74c3fccce18b45fb5f13abb
zaidfadel89 <zaid.fadel89@yahoo.com>
2023-04-17 14:48:14 -0500

    changetheword-2

---
+++ client/build/precache-manifest.81015fa261ac99a16bb0accc2188c697.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "897668468742450f0da16243d33f7ede",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4fdb2296c2f422776fc",
+    "url": "/static/css/2.f3db7d81.chunk.css"
+  },
+  {
+    "revision": "3399b2a1c79da42ab6aa",
+    "url": "/static/css/main.0f978ed9.chunk.css"
+  },
+  {
+    "revision": "a4fdb2296c2f422776fc",
+    "url": "/static/js/2.3b1f881b.chunk.js"
+  },
+  {
+    "revision": "8394a675de9d1495ebc4524cf6dd975c",
+    "url": "/static/js/2.3b1f881b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3399b2a1c79da42ab6aa",
+    "url": "/static/js/main.95b13f2a.chunk.js"
+  },
+  {
+    "revision": "7bda74762d4b3feba78a34074b73d123",
+    "url": "/static/js/main.95b13f2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  },
+  {
+    "revision": "e0ba28fa20ce473fa32aed6fd5a612db",
+    "url": "/static/media/Buy.e0ba28fa.png"
+  },
+  {
+    "revision": "aadd38193598ccd833d2345193856e41",
+    "url": "/static/media/Footer-City.aadd3819.png"
+  },
+  {
+    "revision": "7b7b9591191968b0f4625201d100301a",
+    "url": "/static/media/Rent.7b7b9591.png"
+  },
+  {
+    "revision": "3578f0d80f6edd1b80ebb5be61b00744",
+    "url": "/static/media/Sell.3578f0d8.png"
+  },
+  {
+    "revision": "2631cd03e9aa90a5b21f503a9d1a4ab5",
+    "url": "/static/media/back.2631cd03.jpg"
+  },
+  {
+    "revision": "8d5810fccbb6ffce6311a73fbfe16f6b",
+    "url": "/static/media/chi.8d5810fc.jpg"
+  },
+  {
+    "revision": "b2759c44a4e8c569a0fd53821b502be7",
+    "url": "/static/media/chicago-log.b2759c44.jpg"
+  },
+  {
+    "revision": "76ffe8715f5c90e17693224f5ccb5baf",
+    "url": "/static/media/lognav.76ffe871.png"
+  },
+  {
+    "revision": "aaf8ab5dccafc282d3d3d9820f24c502",
+    "url": "/static/media/paper-1990111.aaf8ab5d.jpg"
+  },
+  {
+    "revision": "bfc071bf1bffc9c2483467f007f305e9",
+    "url": "/static/media/sea-s.bfc071bf.jpg"
+  }
+]);
